e783fffcceb76c3f1ba82a223b9779e0b3c89732
Sara <71656774+SaraCorraless@users.noreply.github.com>
2022-10-08 19:44:00 +0200

    added longitude conversion interface

--- Unit_Converter/app/src/main/java/com/cys/fragmets/LongitudFragment.kt
+++ Unit_Converter/app/src/main/java/com/cys/fragmets/LongitudFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import com.cys.unit_converter.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -32,7 +33,22 @@ class LongitudFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_longitud, container, false)
+        val view:View = inflater.inflate(R.layout.fragment_longitud, container, false)
+        var spinner1 = view.findViewById<Spinner>(R.id.spinner1)
+        var spinner2 = view.findViewById<Spinner>(R.id.spinner2)
+        var editText1 = view.findViewById<EditText>(R.id.editTextNumb1)
+        var textViewResultado = view.findViewById<TextView>(R.id.textViewResultado)
+        var buttonConvertir = view.findViewById<Button>(R.id.buttonConvertir)
+
+
+
+        var adapterSpinner: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(requireContext(), R.array.longitude, android.R.layout.select_dialog_item)
+        spinner1!!.setAdapter(adapterSpinner)
+        spinner2!!.setAdapter(adapterSpinner)
+
+
+
+        return view
     }
 
     companion object {
