22628d67ffc6af936e6046bb3a2665586365f7b6
Sara <71656774+SaraCorraless@users.noreply.github.com>
2022-10-10 18:59:45 +0200

    byte functions

---
+++ Unit_Converter/app/src/main/java/com/cys/utils/Datos.kt
@@ -0,0 +1,39 @@
+package com.cys.utils
+
+class Datos {
+
+    //Byte
+    fun byteKilobyte(byte: Double):Double{
+        println("Paso de byte a kilobyte")
+        return byte / 1000
+    }
+
+    fun byteMegabyte(byte: Double):Double{
+        println("Paso de byte a megabyte")
+        return byte / 1000000
+    }
+
+    fun byteGigabyte(byte: Double):Double{
+        println("Paso de byte a gigabyte")
+        return byte / 1000000000
+    }
+
+    fun byteTerabyte(byte: Double):Double{
+        println("Paso de byte a terabyte")
+        return byte / 1000000000000
+    }
+
+
+    //Kilobyte
+
+
+    //Megabyte
+
+
+    //Gigabyte
+
+
+    //Terabyte
+
+
+}
