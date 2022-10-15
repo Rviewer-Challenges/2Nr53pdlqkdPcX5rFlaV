6a49131f13e2691138e0bc5e233f15aea7e2ed52
Sara <71656774+SaraCorraless@users.noreply.github.com>
2022-10-15 18:42:00 +0200

    Edad fragment

--- Unit_Converter/app/src/main/java/com/cys/fragmets/EdadFragment.kt
+++ Unit_Converter/app/src/main/java/com/cys/fragmets/EdadFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import com.cys.unit_converter.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -30,8 +31,25 @@ class EdadFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edad, container, false)
+        val view:View = inflater.inflate(R.layout.fragment_edad, container, false)
+        var spinner1 = view.findViewById<Spinner>(R.id.spinner1)
+        var spinner2 = view.findViewById<Spinner>(R.id.spinner2)
+        var editText1 = view.findViewById<EditText>(R.id.editTextNumb1)
+        var textViewResultado = view.findViewById<TextView>(R.id.textViewResultado)
+        var buttonConvertir = view.findViewById<Button>(R.id.buttonConvertir)
+
+
+
+        var adapterSpinner: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(requireContext(), R.array.planetas, android.R.layout.select_dialog_item)
+        spinner1!!.setAdapter(adapterSpinner)
+        spinner2!!.setAdapter(adapterSpinner)
+
+        buttonConvertir.setOnClickListener {
+            println("Convertir!")
+            convertir(spinner1, spinner2, textViewResultado, editText1)
+        }
+
+        return view
     }
 
     companion object {
@@ -49,4 +67,48 @@ class EdadFragment : Fragment() {
                 }
             }
     }
+
+    fun convertir(
+        spinner1: Spinner,
+        spinner2: Spinner,
+        textViewResultado: TextView,
+        editText1: EditText
+    ){
+        when(spinner1.selectedItem){
+            "Tierra" ->{
+                convertirTierra(spinner2, textViewResultado, editText1)
+            }
+            "Marte" ->{
+                convertirMarte(spinner2, textViewResultado, editText1)
+            }
+            "Venus" ->{
+                convertirVenus(spinner2, textViewResultado, editText1)
+            }
+            "Mercurio" ->{
+                convertirMercurio(spinner2, textViewResultado, editText1)
+            }
+
+        }
+    }
+
+    private fun convertirMercurio(spinner2: Spinner, textViewResultado: TextView, editText1: EditText) {
+
+
+    }
+
+    private fun convertirVenus(spinner2: Spinner, textViewResultado: TextView, editText1: EditText) {
+
+
+    }
+
+    private fun convertirMarte(spinner2: Spinner, textViewResultado: TextView, editText1: EditText) {
+
+
+    }
+
+    private fun convertirTierra(spinner2: Spinner, textViewResultado: TextView, editText1: EditText) {
+
+
+    }
+
 }
